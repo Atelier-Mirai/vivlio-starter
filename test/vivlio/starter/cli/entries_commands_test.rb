b4e2bfb635d5418c3bc802a8a88597edf9ee8f4c
Atelier Mirai <contact@atelier-mirai.net>
2026-04-12 21:31:25 +0900

    テスト追加

--- test/vivlio/starter/cli/entries_commands_test.rb
+++ test/vivlio/starter/cli/entries_commands_test.rb
@@ -54,6 +54,71 @@ module Vivlio
           end
         end
 
+        # titleタグがない場合はファイル名から番号を除いたものがタイトルになる
+        def test_build_entry_uses_filename_when_no_title_tag
+          Dir.mktmpdir do |dir|
+            path = File.join(dir, '11-quickstart.html')
+            File.write(path, '<html><body>no title</body></html>')
+
+            entry = EntriesCommands.build_entry(path)
+
+            assert_equal 'quickstart', entry[:title]
+          end
+        end
+
+        # titleタグがある場合はそちらが優先される
+        def test_build_entry_prefers_html_title_tag
+          Dir.mktmpdir do |dir|
+            path = File.join(dir, '11-quickstart.html')
+            File.write(path, '<html><title>はじめに</title></html>')
+
+            entry = EntriesCommands.build_entry(path)
+
+            assert_equal 'はじめに', entry[:title]
+          end
+        end
+
+        # パスが ./ で始まっていない場合は正規化される
+        def test_build_entry_normalizes_path_prefix
+          Dir.mktmpdir do |dir|
+            path = File.join(dir, '11-intro.html')
+            File.write(path, '<html></html>')
+
+            entry = EntriesCommands.build_entry(path)
+
+            assert entry[:path].start_with?('./'), "パスが ./ で始まるはずです: #{entry[:path]}"
+          end
+        end
+
+        # 存在しないファイルの場合 extract_html_title は nil を返す
+        def test_extract_html_title_returns_nil_for_missing_file
+          result = EntriesCommands.extract_html_title('/nonexistent/path/file.html')
+
+          assert_nil result
+        end
+
+        # resolve_token は .html 拡張子付きトークンをそのまま解決する
+        def test_resolve_token_with_html_extension
+          Dir.mktmpdir do |dir|
+            File.write(File.join(dir, '11-intro.html'), '')
+
+            result = EntriesCommands.resolve_token(dir, '11-intro.html')
+
+            assert_equal [File.join(dir, '11-intro.html')], result
+          end
+        end
+
+        # resolve_token は拡張子なしトークンをワイルドカードで解決する
+        def test_resolve_token_without_extension_matches_glob
+          Dir.mktmpdir do |dir|
+            File.write(File.join(dir, '11-intro.html'), '')
+
+            result = EntriesCommands.resolve_token(dir, '11-intro')
+
+            assert_includes result, File.join(dir, '11-intro.html')
+          end
+        end
+
         private
 
         def options_context
