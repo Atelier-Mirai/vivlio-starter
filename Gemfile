bf340588f0a05f89c2051901d48e59fbbe9ccdd6
Atelier Mirai <contact@atelier-mirai.net>
2026-04-22 00:02:51 +0900

    logo完成

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gemspec
 
 # query-stream は公開済みバージョンを使用
 # 1.2.1 以降で data/*.yml の safe_load_file 化（堅牢性 11-2 対応）
-gem 'query-stream', '~> 1.2', '>= 1.2.1', path: '../query-stream'
+gem 'query-stream', '~> 1.2', '>= 1.2.1'
 
 # gem install vivlio-starter-pdf を実行したら、以下をコメントアウトしてください
 gem 'vivlio-starter-pdf'
---
+++ copy_to_scaffold.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# sync_scaffold.rb
+# contents/, stylesheets/, config/, codes/, data/, templates/ を
+# lib/project_scaffold/ 以下に上書きコピーする。
+#
+# 使い方: ruby sync_scaffold.rb
+
+require 'fileutils'
+
+SCAFFOLD = File.join(__dir__, 'lib/project_scaffold')
+
+DIRS = %w[contents stylesheets config codes data templates].freeze
+
+DIRS.each do |dir|
+  src = File.join(__dir__, dir)
+  dst = File.join(SCAFFOLD, dir)
+
+  unless Dir.exist?(src)
+    puts "SKIP  #{dir}/ (not found)"
+    next
+  end
+
+  FileUtils.rm_rf(dst)
+  FileUtils.cp_r(src, dst, verbose: false)
+  puts "COPY  #{dir}/ -> lib/project_scaffold/#{dir}/"
+end
+
+FILES = %w[README.md .gitignore].freeze
+
+FILES.each do |file|
+  src = File.join(__dir__, file)
+  dst = File.join(SCAFFOLD, file)
+
+  unless File.exist?(src)
+    puts "SKIP  #{file} (not found)"
+    next
+  end
+
+  FileUtils.cp(src, dst, verbose: false)
+  puts "COPY  #{file} -> lib/project_scaffold/#{file}"
+end
+
+puts "\nDone."
